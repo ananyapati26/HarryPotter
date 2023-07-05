da908a87abc651e99041164d8fcc2c3af5b9a903
Devansh <120627516+Devanshbansal02@users.noreply.github.com>
2023-07-06 00:08:36 +0530

    Add files via upload

---
+++ app_characters.js
@@ -0,0 +1,22 @@
+fetch("https://hp-api.onrender.com/api/characters").then((data)=>{
+    return data.json();
+})
+.then((objectData)=>{
+    console.log(objectData[0].name);
+    console.log(objectData);
+    let tableData="";
+    objectData.map((values)=>{
+        tableData+=`          <tr>
+        <td class="img"><img src="${values.image}" alt="error"></td>
+        <td class="name">${values.name}</td>
+        <td class="species">${values.species}</td>
+        <td class="actor">${values.actor}</td>
+        </tr>`;
+        // count+=1;
+        // if(count>=4){
+            
+        // }
+});
+document.getElementById("table_body").innerHTML=tableData;
+})
+let count=0;
