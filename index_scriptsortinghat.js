7362a348ec18efa2f88b94839f3d248ce726cb71
Ananya Pati <126486682+ananyapati26@users.noreply.github.com>
2023-07-27 15:31:48 +0530

    Update index_scriptsortinghat.js

--- index_scriptsortinghat.js
+++ index_scriptsortinghat.js
@@ -116,32 +116,32 @@ function submit()
   }
   if(g>r && g>h && g==s)
   {
-    alert("OOPS PLAY AGAIN");
+    alert("OOPS ITS A TIE , PLAY AGAIN");
     window.location="index_sorting hat.html"
   }
   if(g>r && g>s && g==h)
   {
-    alert("OOPS PLAY AGAIN");
+    alert("OOPS ITS A TIE , PLAY AGAIN");
     window.location="index_sorting hat.html"
   }
   if(g>s && g>h && g==r)
   {
-    alert("OOPS PLAY AGAIN");
+    alert("OOPS ITS A TIE , PLAY AGAIN");
     window.location="index_sorting hat.html"
   }
   if(s>r && s>g && s==h)
   {
-    alert("OOPS PLAY AGAIN");
+    alert("OOPS ITS A TIE , PLAY AGAIN");
     window.location="index_sorting hat.html"
   }
   if(s>g && s>h && r==s)
   {
-    alert("OOPS PLAY AGAIN");
+    alert("OOPS ITS A TIE , PLAY AGAIN");
     window.location="index_sorting hat.html"
   }
   if(r>g && r>s && r==h)
   {
-    alert("OOPS PLAY AGAIN");
+    alert("OOPS ITS A TIE , PLAY AGAIN");
     window.location="index_sorting hat.html"
   }
      
